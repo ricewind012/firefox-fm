a3aa7784bfaf1d777065f1b1bb83deb172e112f5
ricewind012 <58827198+ricewind012@users.noreply.github.com>
2025-06-24 14:45:13 +0000

    feat: generate firefox frontend types

--- types/DownloadUtils.d.ts
+++
@@ -1,6 +0,0 @@
-declare module "resource://gre/modules/DownloadUtils.sys.mjs" {
-	export const DownloadUtils: {
-		/** @returns [convertedBytes, units] */
-		convertByteUnits(bytes: number): [number, string];
-	};
-}
